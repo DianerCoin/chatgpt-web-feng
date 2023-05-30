4e3230b9784c8287f98611966e91ae7bb85f4099
xuefeng <xuefeng@baoku.com>
2023-05-30 14:18:50 +0800

    变更姓名，增加备案号

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'xuefeng',
+      description: '备案号: <a href="https://beian.miit.gov.cn/" class="text-blue-500" target="_blank" >京ICP备2021036736号</a>',
     },
   }
 }
